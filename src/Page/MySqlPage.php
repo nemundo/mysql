011ac8f7c81bbcee77f4e000e87e2ee508d8c9d0
unknown <urs.lang@gmail.com>
2020-08-06 10:25:36 +0200

    .

--- src/Page/MySqlPage.php
+++ src/Page/MySqlPage.php
@@ -2,12 +2,14 @@
 
 namespace Nemundo\MySql\Page;
 
-use Nemundo\Admin\MySql\Com\MySqlTableListBox;
-use Nemundo\Admin\MySql\Form\MySqlDatabaseForm;
+use Nemundo\Admin\Com\Table\AdminTable;
+use Nemundo\Com\TableBuilder\TableHeader;
+use Nemundo\Com\TableBuilder\TableRow;
 use Nemundo\Com\Template\AbstractTemplateDocument;
+use Nemundo\Db\Provider\MySql\Connection\MySqlConnection;
+use Nemundo\Db\Provider\MySql\Database\MySqlDatabaseReader;
 use Nemundo\Html\Paragraph\Paragraph;
 use Nemundo\MySql\Com\Form\SqlFileImportForm;
-use Nemundo\Project\Path\TmpPath;
 
 class MySqlPage extends AbstractTemplateDocument
 {
@@ -15,20 +17,35 @@ class MySqlPage extends AbstractTemplateDocument
     public function getContent()
     {
 
-        $p=new Paragraph($this);
-        $p->content='MySql';
+        $p = new Paragraph($this);
+        $p->content = 'MySql';
 
         //$p=new Paragraph($this);
         //$p->content='Tmp Path: '.(new TmpPath())->getPath();
 
 
-
         new SqlFileImportForm($this);
 
 
+        $table = new AdminTable($this);
+
+        $header = new TableHeader($table);
+        $header->addText('Database');
 
 
+        $conn = new MySqlConnection();
+        $conn->connectionParameter->host = 'localhost';
+        $conn->connectionParameter->port = '3306';
+        $conn->connectionParameter->user = 'root';
+        $conn->connectionParameter->password = '';
 
+
+        $reader = new MySqlDatabaseReader();
+        $reader->connection = $conn;
+        foreach ($reader->getData() as $database) {
+            $row = new TableRow($table);
+            $row->addText($database->databaseName);
+        }
 
 
         return parent::getContent();
